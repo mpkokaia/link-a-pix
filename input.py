1fbb81835cae3c7a2d5b78cef8bbd17296ddeb16
kopeicev <kopeicev@kasperskyclub.com>
2013-12-03 13:12:52 +0600

    code merge started

--- input.py
+++ input.py
@@ -1,8 +1,137 @@
-from copy import deepcopy
+import copy
 
+class Step:
+    def __init__(self, inp):
+        self.inp=inp
+        self.output=[]
+        self.output_tmp=[]
+    def step_right(self,i,j):
+        if(i==(len(self.inp[0])-1)):
+            return False
+        elif (self.inp[i+1][j][1]==1):
+            return False
+        else:
+            return [i+1,j]
+
+    def step_left(self,i,j):
+        if(i==0):
+            return False
+        elif (self.inp[i-1][j][1]==1):
+            return False
+        else:
+            return [i-1,j]
+
+    def step_up(self,i,j):
+        if(j==0):
+            return False
+        elif (self.inp[i][j-1][1]==1):
+            return False
+        else:
+            return [i,j-1]
+
+    def step_down(self,i,j):
+        if(j==(len(self.inp[0])-1)):
+            return False
+        elif (self.inp[i][j+1][1]==1):
+            return False
+        else:
+            return [i,j+1]
+
+    def choosee_step1(self,index_operation,i,j):
+        operations=[self.step_right,self.step_down,self.step_left,self.step_up]
+        for p in range(index_operation,4):
+            f=operations[p](i,j)
+            if (f!=False):
+                return [p,f]
+        return False
+
+    def choosee_step(self,index_operation,i,j,prev):
+        operations=[self.step_right,self.step_down,self.step_left,self.step_up]
+        for p in range(index_operation,4):
+            if(prev==0):
+                if(p==2):
+                    continue
+            elif(prev==1):
+                if(p==3):
+                    continue
+            elif(prev==2):
+                if(p==0):
+                    continue
+            elif(prev==3):
+                if(p==1):
+                    continue
+            f=operations[p](i,j)
+            if (f!=False):
+                return [p,f]
+        return False
+
+    def look_for_the_route(self,i,j,l,directions,route,var):
+        i0=i
+        j0=j
+        index_operation=0
+        p=0
+        prev=-1
+        if(len(directions)!=0):
+            index_operation=directions.pop()+1
+            p=l-2
+            route.pop()
+            if(len(directions)!=0):
+                prev=directions[-1]
+                i=route[-1][0]
+                j=route[-1][1]
+            else:
+                prev=-1
+                i=i0
+                j=j0
+
+        while ((p< (l-1) ) and (p!=-1) ):
+            data=self.choosee_step(index_operation,i,j,prev)
+            if(data!=False):
+                p+=1
+                i=data[1][0]
+                j=data[1][1]
+                route.append(copy.copy([i,j]))
+                directions.append(copy.copy(data[0]))
+                prev=data[0]
+                index_operation=0
+            else:
+                p-=1
+                if (p!=-1):
+                    index_operation=directions.pop()+1
+                    route.pop()
+                    if(len(route)!=0):
+                        prev=directions[-1]
+                        i=route[-1][0]
+                        j=route[-1][1]
+                    else:
+                        prev=-1
+                        i=i0
+                        j=j0
+        if (p==-1):
+            self.output_tmp.append(copy.copy(var))
+            return var
+        elif (self.inp[i][j][0]==l):
+            var.append(copy.copy(route))
+            self.look_for_the_route(i0,j0,l,directions,route,var)
+            return var
+        else:
+            self.look_for_the_route(i0,j0,l,directions,route,var)
+
+    def write_output(self,i,j):
+        if(self.inp[i][j][0]>0):
+            self.look_for_the_route(i,j,self.inp[i][j][0],[],[],[])
+        else:
+            self.output_tmp.append([])
+        self.output=copy.copy(self.output_tmp)
+        self.output_tmp=[]
+        return self.output.pop()
+
+#if (self.inp[i][j][0]==1):
+#self.output_tmp.append([i,j])
+#else:
 
 class linkapix():
-    condition = [[]]
+    condition = [[[]]]
     variants = [[[]]]
     height = 0
     width = 0
@@ -17,22 +146,22 @@ class linkapix():
         for i in data:
             if i == '\n':
                 if not prevNumber:
-                    tempdata.append(0)
+                    tempdata.append([0, 0])
                 self.condition.append(tempdata)
                 prevNumber = False
                 tempdata = []
                 continue
 
             if not i == ',':
-                tempdata.append(int(i))
+                tempdata.append([int(i), 0])
                 prevNumber = True
             else:
                 if prevNumber:
                     prevNumber = False
                 else:
-                    tempdata.append(0)
+                    tempdata.append([0, 0])
         if not prevNumber:
-            tempdata.append(0)
+            tempdata.append([0, 0])
         self.condition.append(tempdata)
         self.condition.remove(self.condition[0])
 
@@ -41,7 +170,7 @@ class linkapix():
         self.height = len(self.condition)
         self.width = len(self.condition[0])
         for i in range(self.height):
-            for ii in (range(self.width) - 1):
+            for ii in range(self.width-1):
                 tempdata.append([])
             self.variants.append(tempdata)
             tempdata = [[]]
@@ -49,9 +178,13 @@ class linkapix():
 
 #функция заполняет массив всевозможных вариантов для каждого элемента кроссворда
 def getVariants(crossword):
-    #WARNING!!! нужно сюда привязать код Маши
-    return
-
+    for i in range(crossword.height):
+        for j in range(crossword.width):
+            if (not crossword.condition[i][j][0] == 0) and (not crossword.condition[i][j][1] == 1):
+                crossword.variants[i][j] = Step(crossword.condition).write_output(i, j)
+                if len(crossword.variants[i][j]) == 0:
+                    return False
+    return True
 
 #функция, которая закрашивает клетки по указанному маршруту
 def paint(crossword, route):
@@ -70,12 +203,18 @@ def paintFirstOneVariants(crossword):
         return True
     return False
 
+def heurPredetect(crossword):
+    for i in crossword.condition:
+        for ii in i:
+            if ii[0] == 1:
+                ii[1] = 1
+
 #рекурсивная функция, основная
 def solveLinkAPix(crossword):
     newcrossword = linkapix()
-    newcrossword.condition = deepcopy(crossword.condition)
-    newcrossword.height = deepcopy(crossword.height)
-    newcrossword.width = deepcopy(crossword.width)
+    newcrossword.condition = copy.deepcopy(crossword.condition)
+    newcrossword.height = copy.deepcopy(crossword.height)
+    newcrossword.width = copy.deepcopy(crossword.width)
     #назаполняем варианты с единственными решениями
     again = True
     while again:
@@ -86,12 +225,15 @@ def solveLinkAPix(crossword):
             return False
 
         #пока заполняется, заполняем варианты с единственным решением, по одному за итерацию цикла
-        if paintFirstOneVariants(newcrossword):
-            again = True
+        #if paintFirstOneVariants(newcrossword):
+        #    again = True
     #WARNING!!! функция недописана
 
 crossword = linkapix()
 #читаем входной файл и парсим данные из него
 crossword.init('sample1.txt')
+heurPredetect(crossword)
 #пытаемся решить
 solved = solveLinkAPix(crossword)
+for i in crossword.condition:
+    print(i);
